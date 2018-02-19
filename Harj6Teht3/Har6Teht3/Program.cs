412d99a74fda94876c66dae93e105e7056507280
MJarvila <35224611+MJarvila@users.noreply.github.com>
2018-02-19 20:14:29 +0200

    Add files via upload

---
+++ Harj6Teht3/Har6Teht3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Kannettavassa radiossa on vain kolme säädintä: päälle/pois-kytkin, 
+/// äänen voimakkuuden säädin (arvot 0, 1, 2,..., 9) ja kuunneltavan kanavan taajuusvalinta (2000.0 - 26000.0). 
+/// Laadi luokka kannettavan radion toteutukseksi. 
+/// Käytä tekemääsi luokkaa pääohjelmasta käsin, eli testaile radion toimivuutta erilaisilla voimakkuuden ja taajuuden arvoilla. 
+/// Jätä asetus- ja tulostuslauseet näkyville pääohjelmaan, jotta radio-olion käyttö voidaan todentaa. 
+/// </summary>
+namespace Har6Teht3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio Testi = new Radio();
+            Console.WriteLine("Decide if the radio is on or off by typing true or false: ");
+            Testi.OnOff = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the volume from a range between 0 and 9: ");
+            Testi.Volume = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the desired frequency from a range between 2000.0 - 26000.0 (Use a comma): ");
+            Testi.Freq = double.Parse(Console.ReadLine());
+            Testi.LookAtRadio();
+        }
+    }
+}
---
+++ Harj6Teht3/Har6Teht3/Radio.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Kannettavassa radiossa on vain kolme säädintä: päälle/pois-kytkin, 
+/// äänen voimakkuuden säädin (arvot 0, 1, 2,..., 9) ja kuunneltavan kanavan taajuusvalinta (2000.0 - 26000.0). 
+/// Laadi luokka kannettavan radion toteutukseksi. 
+/// Käytä tekemääsi luokkaa pääohjelmasta käsin, eli testaile radion toimivuutta erilaisilla voimakkuuden ja taajuuden arvoilla. 
+/// Jätä asetus- ja tulostuslauseet näkyville pääohjelmaan, jotta radio-olion käyttö voidaan todentaa. 
+/// </summary>
+namespace Har6Teht3
+{
+    class Radio
+    {
+        public bool OnOff { get; set; }
+        private int MinVolume = 0;
+        private int MaxVolume = 9;
+        public int volume;
+        public int Volume
+        {
+            get { return volume; }
+            set
+            {
+ 
+                if (value >= MinVolume && value <= MaxVolume)
+                {
+                    volume = value;
+                    //Console.WriteLine("JeeJee");
+                }
+                else
+                    Console.WriteLine("Values only from 0 to 9!");
+            }
+        }
+        private double MinFreq = 2000.0;
+        private double MaxFreq = 26000.0;
+        public double freq;
+        public double Freq
+        {
+            get { return freq; }
+            set
+            {
+                if (value >= MinFreq && value <= MaxFreq)
+                {
+                    freq = value;
+                    //Console.WriteLine("Radio kuuluu ja homma toimii.");
+                }
+                else
+                    Console.WriteLine("Not within the valid frequency range!");
+            }
+        }
+        public void LookAtRadio()
+        {
+            Console.WriteLine("The radio is on: " + OnOff + "\n" + "Volume is: " + Volume + "\n" + "The frequency is: " + Freq);
+        }
+    }
+}
